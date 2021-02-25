0eb15c48c860b327b780d40d51638e0d7f92f677
Daniel G <dgliddy@gmail.com>
2021-02-25 15:57:01 +1300

    Update graviton.py

--- graviton.py
+++ graviton.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+import json
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def rt_default():
+    resp = {
+      'data': [{
+        'type': 'response',
+        'id': '1',
+        'attributes': {
+          'status': "success"}
+      }]
+    }
+    resp_json = json.dumps(resp)
+    return Response(resp_json, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
