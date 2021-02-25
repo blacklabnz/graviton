f9a4c3d5b17fdc528cb18efaa8df54d1af0c494c
Daniel G <dgliddy@gmail.com>
2021-02-25 19:44:16 +1300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+az container create \
+    --name aci-flaskapi \
+    --resource-group rg-flaskapi-dev \
+    --image python \
+    --dns-name-label aci-flaskapi-$RANDOM \
+    --query ipAddress.fqdn \
+    --ports 80 \
+    --gitrepo-url https://github.com/dg-hub/graviton.git \
+    --gitrepo-mount-path /mnt/graviton \
+    --command-line "/bin/bash -c '/mnt/graviton/run.sh'" \
+    --restart-policy Never \
+    --cpu 1 \
+    --memory 1
